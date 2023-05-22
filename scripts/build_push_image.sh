2cc9db22f121d0c6031f25983a1a1ee48a9a1197
figoperdana <192410102022@mail.unej.ac.id>
2023-05-23 00:51:06 +0700

    update

--- scripts/build_push_image.sh
+++ scripts/build_push_image.sh
@@ -2,6 +2,6 @@
 
 docker build -t item-app:v1 .
 docker image ls
-docker tag item-app:v1 figoperdana/item-app:v1
+docker tag item-app:v1 dafit17docker/item-app:v1
 docker login
-docker push figoperdana/item-app:v1
+docker push dafit17docker/item-app:v1
