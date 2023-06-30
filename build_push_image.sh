d6c899fc82fe94f752458a134f8c4c7527d65f87
figoperdana <192410102022@mail.unej.ac.id>
2023-06-30 11:37:29 +0700

    update

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Membuat image Docker dengan nama "karsajobs-ui" dan tag "latest" dari Dockerfile di direktori saat ini
 docker build -t karsajobs-ui:latest .
+
+# Menampilkan daftar image Docker yang ada di komputer lokal Anda
 docker image ls
+
+# Jadi dalam hal ini, kita membuat tag dengan hostname "ghcr.io", user "figoperdana", dan repository "karsajobs-ui"
 docker tag karsajobs-ui:latest ghcr.io/figoperdana/karsajobs-ui:latest
 
-# need variable CR_PAT, it can be get from github personal access token
-# echo $secrets.GHCR_TOKEN | docker login ghcr.io -u figoperdana --password-stdin
-# echo "$GHCR_TOKEN" | docker login ghcr.io -u "figoperdana" --password-stdin
-# docker login ghcr.io -u figoperdana -p $secrets.GHCR_TOKEN
-docker login ghcr.io -u figoperdana -p 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
+# Melakukan login ke GitHub Container Registry (ghcr.io) menggunakan username "figoperdana" dan token autentikasi dari variabel lingkungan $GHCR_TOKEN
+docker login ghcr.io -u figoperdana -p $GHCR_TOKEN
 
+# Melakukan push image Docker dengan tag "ghcr.io/figoperdana/karsajobs-ui:latest" ke GitHub Container Registry (ghcr.io)
 docker push ghcr.io/figoperdana/karsajobs-ui:latest
